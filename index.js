2f6c2e7b37b495a5b2a01f862efd8616bb7428eb
tim <pub@lalatte.com>
2016-04-30 14:40:10 +0800

    faster camera and configuration

--- index.js
+++ index.js
@@ -22,17 +22,16 @@ var configIoT = {
 var thingShadow = awsIot.thingShadow(configIoT);
 
 var thingState = {
-    ip: null
+    ip: null,
+    tweet: 'Hi from ' + config.iotClientId,
+    cameraRotation: 0
 };
-
 
 var os = require('os');
 var ifaces = os.networkInterfaces();
 setInterval(function() {
 
     ifaces.wlan0.forEach(function(iface) {
-
-	// console.log(iface.family, iface.address);
 
         if (iface.family == 'IPv4') {
 
@@ -74,58 +73,66 @@ thingShadow.on('reconnect', function() {
     });
 });  
 
+thingShadow.on('delta', function(thingName, stateObject) {
+    // received thingShadow delta on pi-camera: {"timestamp":1461996510,"state":{"tweet":"Hi from pi-camera test"},"metadata":{"tweet":{"timestamp":1461996484}}}
+    console.log('received thingShadow delta on ' + thingName+': ' + JSON.stringify(stateObject));
+
+    if (stateObject.state.tweet) thingState.tweet = stateObject.state.tweet;
+    if (stateObject.state.cameraRotation) thingState.cameraRotation = stateObject.state.cameraRotation;
+});
+
+
 thingShadow.on('message', function(topic, payload) {
 
     console.log('Message received on topic', topic, 'with message', payload.toString());
 
-  if (topic == 'chip/button') {
+    if (topic == 'chip/button') {
 
-    var filename = Date.now() + '.jpg';
+      var filename = Date.now() + '.jpg';
 
-    cam.prepare({
-        timeout: 0,
-        quality: 85,
-        width: 800,
-        height: 600
-    }).takePicture(filename, function(file, err) {
+      cam.prepare({
+          timeout: 10,
+          quality: config.cameraQuality || 85,
+          width: config.cameraWidth || 800,
+          height: config.cameraHeight || 600,
+          rotation: thingState.cameraRotation
+      }).takePicture(filename, function(file, err) {
 
-        if (!err) {
+          if (!err) {
 
-            console.log('Tacking picture to', file);
+              console.log('Tacking picture to', file);
 
-            var fileBuffer = fs.readFileSync(config.localStorage + '/' + filename);
+              var fileBuffer = fs.readFileSync(config.localStorage + '/' + filename);
 
-            var key = '';
-            if (config.s3BucketFolder && config.s3BucketFolder.length > 0) key += config.s3BucketFolder + '/';
-            key += filename;
+              var key = '';
+              if (config.s3BucketFolder && config.s3BucketFolder.length > 0) key += config.s3BucketFolder + '/';
+              key += filename;
             
-            var bucket = config.s3Bucket;
-//            if (config.s3BucketFolder && config.s3BucketFolder.length > 0) bucket += '/' + config.s3BucketFolder;       
+              var bucket = config.s3Bucket;
+//              if (config.s3BucketFolder && config.s3BucketFolder.length > 0) bucket += '/' + config.s3BucketFolder;       
 
-            s3Client.putObject({
-                ACL: 'public-read',
-                Bucket: bucket, //config.s3Bucket,
-                Key: key,
-                Body: fileBuffer,
-                ContentType: 'image/jpg'
-            }, function(error, response) {
-                if (!error) {
-                  console.log('Upload to S3 finished', arguments);
-                  console.log('Publishing to', config.iotPublishTopic);
-                  thingShadow.publish(config.iotPublishTopic, JSON.stringify({filename: key}));
-                } else {
-                  console.error('ERROR', error);
-                }
-            });
+              s3Client.putObject({
+                  ACL: 'public-read',
+                  Bucket: bucket, //config.s3Bucket,
+                  Key: key,
+                  Body: fileBuffer,
+                  ContentType: 'image/jpg'
+              }, function(error, response) {
+                  if (!error) {
+                    console.log('Upload to S3 finished', arguments);
+                    console.log('Publishing to', config.iotPublishTopic);
+                    thingShadow.publish(config.iotPublishTopic, JSON.stringify({filename: key, tweet: thingState.tweet}));
+                  } else {
+                    console.error('ERROR', error);
+                  }
+              });
 
-        } else {
+          } else {
+              console.error('ERROR: ', err);
+          }
 
-            console.error('ERROR: ', err);
-        }
+      });
 
-
-    });
-
-  }
+    }
 
 });
